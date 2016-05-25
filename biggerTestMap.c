b4da51b098258a481ae45eabbd68b600f3c7d841
atiredturtle <guy@segev.com>
2016-05-25 18:05:51 +1000

    Update biggerTestMap.c

--- biggerTestMap.c
+++ biggerTestMap.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,9 +11,9 @@ typedef struct _node {
    int ID;
    // all links
    link link1;
-   link link2; 
+   link link2;
    link link3;
-   
+
    int owner1;
    int owner2;
    int owner3;
@@ -29,113 +30,69 @@ typedef struct _node {
 */
 
 void joinNodes (link n, link l1, link l2, link l3);
-void assignID (link *map);
+void populateMap (link *map);
 void printMap (link map[]);
 
 int main (int argc, char *argv[]){
    link map[NUM_NODES];
-   
-   // create all nodes
-   link node1  = malloc(sizeof(node));
-   link node2  = malloc(sizeof(node));
-   link node3  = malloc(sizeof(node));
-   link node4  = malloc(sizeof(node));
-   link node5  = malloc(sizeof(node));
-   link node6  = malloc(sizeof(node));
-   link node7  = malloc(sizeof(node));
-   link node8  = malloc(sizeof(node));
-   link node9  = malloc(sizeof(node));
-   link node10 = malloc(sizeof(node));
-   link node11 = malloc(sizeof(node));
-   link node12 = malloc(sizeof(node));
-   link node13 = malloc(sizeof(node));
-   
-   // place all nodes in map array
-   map[0]  = node1;
-   map[1]  = node2;
-   map[2]  = node3;
-   map[3]  = node4;
-   map[4]  = node5;
-   map[5]  = node6;
-   map[6]  = node7;
-   map[7]  = node8;
-   map[8]  = node9;
-   map[9]  = node10;
-   map[10] = node11;
-   map[11] = node12;
-   map[12] = node13;
-   
-   assignID (map);
-   
-   printf ("\n \n");
-   
-   /* list of connections
-   node1 -> node2    
-   node1 -> node7    
-   node2 -> node3    
-   node3 -> node4    
-   node3 -> node9    
-   node4 -> node5    
-   node5 -> node6    
-   node7 -> node8    
-   node8 -> node9    
-   node8 -> node11     
-   node9 -> node10      
-   node10 -> node6     
-   node10 -> node13  
-   node11 -> node12  
-   node12 -> node13 
-   */
-   
-   /* association table for joining nodes (0 is no link)
-      |  link1  |  link2  |  link3  |                          
-   1  |    0    |    2    |    7    |                          
-   2  |    0    |    3    |    1    |
-   3  |    2    |    4    |    9    |
-   4  |    0    |    5    |    3    |
-   5  |    4    |    0    |    6    |
-   6  |    5    |    0    |    10   |
-   7  |    1    |    8    |    0    |
-   8  |    7    |    9    |    11   |
-   9  |    3    |    10   |    8    |
-   10 |    9    |    6    |    13   |
-   11 |    8    |    12   |    0    |
-   12 |    11   |    13   |    0    |
-   13 |    10   |    0    |    12   |
-   */   
-   
 
-   
+   populateMap(map);
+
+   printf ("\n \n");
+
+   /* association table for joining nodes (? is no link)
+      |  link1  |  link2  |  link3  |
+   0  |    ?    |    1    |    6    |
+   1  |    ?    |    2    |    0    |
+   2  |    1    |    3    |    8    |
+   3  |    ?    |    4    |    2    |
+   4  |    3    |    ?    |    5    |
+   5  |    4    |    ?    |    9    |
+   6  |    0    |    7    |    ?    |
+   7  |    6    |    8    |    10   |
+   8  |    3    |    10   |    8    |
+   9  |    8    |    5    |    12   |
+   10 |    7    |    11   |    ?    |
+   11 |    10   |    12   |    ?    |
+   12 |    9    |    ?    |    11   |
+   */
+
+
+
    // join all nodes
-   joinNodes (node1, NULL, node2, node7);
-   joinNodes (node2, NULL, node3, node1);
-   joinNodes (node3, node2, node4, node9);
-   joinNodes (node4, NULL, node5, node3);
-   joinNodes (node5, node4, NULL, node6);
-   joinNodes (node6, node5, NULL, node10);
-   joinNodes (node7, node1, node8, NULL);
-   joinNodes (node8, node7, node9, node11);
-   joinNodes (node9, node3, node10, node8);
-   joinNodes (node10, node9, node6, node13);
-   joinNodes (node11, node8, node12, NULL);
-   joinNodes (node12, node11, node13, NULL);
-   joinNodes (node13, node10, NULL, node12);
-   
+   joinNodes (map[0], NULL, map[1], map[6]);
+   joinNodes (map[1], NULL, map[2], map[0]);
+   joinNodes (map[2], map[1], map[3], map[8]);
+   joinNodes (map[3], NULL, map[4], map[2]);
+   joinNodes (map[4], map[3], NULL, map[5]);
+   joinNodes (map[5], map[4], NULL, map[9]);
+   joinNodes (map[6], map[0], map[7], NULL);
+   joinNodes (map[7], map[6], map[8], map[10]);
+   joinNodes (map[8], map[2], map[9], map[7]);
+   joinNodes (map[9], map[8], map[5], map[12]);
+   joinNodes (map[10], map[7], map[11], NULL);
+   joinNodes (map[11], map[10], map[12], NULL);
+   joinNodes (map[12], map[9], NULL, map[11]);
+
    printMap (map);
-   
+
    return EXIT_SUCCESS;
 }
 
 void joinNodes (link n, link l1, link l2, link l3){
    n->link1 = l1;
    n->link2 = l2;
-   n->link3 = l3; 
+   n->link3 = l3;
 }
 
-void assignID (link *map){
+void populateMap (link *map){
    int i = 0;
+
    while (i < NUM_NODES){
-      map[i]->ID = i+1;
+      // create new node
+      map[i] = malloc(sizeof(node));
+      // set the ID for the node
+      map[i]->ID = i;
       i++;
    }
 }
@@ -143,24 +100,24 @@ void assignID (link *map){
 void printMap (link map[NUM_NODES]){
     int i = 0;
     while (i < NUM_NODES){
-        printf ("node%d:   ", i+1);
+        printf ("node%d:   ", i);
         if (map[i]->link1 != NULL){
-           printf ("link1 -> %d    |  ", map[i]->link1->ID);   
+           printf ("link1 -> %d    |  ", map[i]->link1->ID);
         } else {
            printf ("link1 -> NULL  |  ");
         }
-        
+
         if (map[i]->link2 != NULL){
-           printf ("link2 -> %d    |  ", map[i]->link2->ID);   
+           printf ("link2 -> %d    |  ", map[i]->link2->ID);
         } else {
            printf ("link2 -> NULL  |  ");
         }
-        
+
         if (map[i]->link3 != NULL){
-           printf ("link3 -> %d    |  \n", map[i]->link3->ID);   
+           printf ("link3 -> %d    |  \n", map[i]->link3->ID);
         } else {
            printf ("link3 -> NULL  |  \n");
-        }        
+        }
         i++;
     }
 }
